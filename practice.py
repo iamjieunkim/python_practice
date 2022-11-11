d207308e6dc8639637b3a9ef1ed290aca8c71c8d
jieun <jek3118@naver.com>
2022-11-12 00:18:21 +0900

    22

--- practice.py
+++ practice.py
@@ -1,9 +1,6 @@
-#문자열을 숫자로 바꾸기
-string_a=input("입력A> ")
-int_a=int(string_a)
+#int() 함수와 float() 함수 활용하기
+output_a=int("52")
+output_b=float("52.273")
 
-string_b=input("입력B> ")
-int_b=int(string_b)
-
-print("문자열 자료:", string_a+string_b)
-print("숫자 자료:", int_a+int_b)
+print(type(output_a), output_a)
+print(type(output_b), output_b)
