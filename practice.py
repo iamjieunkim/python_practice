5c94cd6abd7e7b01ca03aeaf8f8b42aa6149e2f6
jieun <jek3118@naver.com>
2022-11-12 00:43:09 +0900

    28

--- practice.py
+++ practice.py
@@ -1,6 +1,11 @@
+#format() 함수의 다양한 형태
 #format() 함수로 숫자를 문자열로 변환하기
-string_a="{}".format(10)
+format_a="{}만 원".format(5000)
+format_b="파이썬 열공하여 첫 연봉 {}만 원 만들기".format(6000)
+format_c="{} {} {}".format(3000, 4000, 5000)
+format_d="{} {} {}".format(1, "문자열", True)
 
-#출력하기
-print(string_a)
-print(type(string_a))
+print(format_a)
+print(format_b)
+print(format_c)
+print(format_d)
