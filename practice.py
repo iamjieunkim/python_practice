249796d8740d1052c639c4fb03b3114a75e71520
jieun <jek3118@naver.com>
2022-11-12 00:01:40 +0900

    17

--- practice.py
+++ practice.py
@@ -1,9 +1,12 @@
-#변수 선언과 할당
-pi=3.14159265
-r=10
+#복합 대입 연산자
+number=100
+number+=10
+number+=20
+number+=30
+print("number:", number)
 
-#변수 참조
-print("원주율=", pi)
-print("반지름=", r)
-print("원의 둘레=", 2*pi*r) #원의 둘레
-print("원의 넓이=", pi*r*r) #원의 넓이
+#문자열 복합 대입 연산자
+string="안녕하세요"
+string+="!"
+string+="!"
+print("string:", string)
