632c098785fddd13f04e180a2cb2892eb9b14722
jieun <jek3118@naver.com>
2022-11-11 23:15:16 +0900

    4

--- practice.py
+++ practice.py
@@ -1,9 +1,4 @@
-# 하나만 출력합니다.
-print('#하나만 출력합니다.')
-print("Hello Python Programming...!")
-print()
-
-#여러 개를 출력합니다.
-print('#여러 개를 출력합니다.')
-print(10, 20, 30, 40, 50)
-print("안녕하세요", "저의", "이름은")
+#자료형 확인하기
+print(type("안녕하세요"))
+print(type(263))
+print(type(5>3))
