7515f8890c77f22e3a1fd9e49366751dbd0586ae
jieun <jek3118@naver.com>
2022-11-12 00:16:00 +0900

    21

--- practice.py
+++ practice.py
@@ -1,5 +1,9 @@
-#입력자료형 확인하기
-string = input("입력>")
+#문자열을 숫자로 바꾸기
+string_a=input("입력A> ")
+int_a=int(string_a)
 
-print("자료: ", string)
-print("자료형: ", type(string))
+string_b=input("입력B> ")
+int_b=int(string_b)
+
+print("문자열 자료:", string_a+string_b)
+print("숫자 자료:", int_a+int_b)
