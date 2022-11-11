3dc460e38700b657143c399775122eae363b05a9
jieun <jek3118@naver.com>
2022-11-12 00:09:00 +0900

    19

--- practice.py
+++ practice.py
@@ -1,9 +1,12 @@
-#사용자 입력:input()
-print("#사용자 입력:input()")
-input("인사말을 입력하세요> ")
-
-
 #input()함수로 사용자 입력받기
 print("#input()함수로 사용자 입력받기")
 string=input("인사말을 입력하세요> ")
 print(string)
+print(type(string))
+
+#input()함수로 사용자 입력받기
+
+print("#input()함수로 사용자 입력받기")
+number=input("숫자를 입력하세요> ")
+print(number)
+print(type(number))
