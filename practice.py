496c5b9f018a94e36e151fb753a9a099e8db71cc
jieun <jek3118@naver.com>
2022-11-12 01:32:10 +0900

    37

--- practice.py
+++ practice.py
@@ -1,9 +1,3 @@
-#문자열 찾기: find()와 rfind()
-output_a="안녕안녕하세요".find("안녕")
-print(output_a)
-
-output_b="안녕안녕하세요".rfind("안녕")
-print(output_b)
-
-#find():왼쪽부터 찾아서 처음 등장하는 위치를 찾습니다.
-#rfind():오른쪽부터 찾아서 처음 등장하는 위치를 찾습니다.
+#문자열 자르기: split()
+a="10 20 30 40 50".split(" ")
+print(a)
