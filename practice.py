8cd77e4b15815d9451904378dff1cdb7244c7adc
jieun <jek3118@naver.com>
2022-11-12 00:55:13 +0900

    29

--- practice.py
+++ practice.py
@@ -6,7 +6,7 @@ output_a= "{:d}".format(52)
 output_b= "{:5d}".format(52)
 output_c= "{:10d}".format(52)
 
-#빈칸을 0으로 채우기
+#빈칸을 0으로 채우기.
 output_d= "{:05d}".format(52)
 output_e= "{:05d}".format(-52)
 
@@ -20,3 +20,4 @@ print(output_c)
 print("#빈칸을 0으로 채우기")
 print(output_d)
 print(output_e)
+
