f031f2492830dbebdbd7bb1dc9ee575df715a22a
jieun <jek3118@naver.com>
2022-11-12 01:14:32 +0900

    32

--- practice.py
+++ practice.py
@@ -1,15 +1,30 @@
 #기호와 함께 출력하기
-output_h="{: d}".format(52)
-output_i="{: d}".format(-52)
-output_j="{:=+5d}".format(52)
-output_k="{:=+5d}".format(-52)
-output_l="{:+05d}".format(52)
-output_m="{:+05d}".format(-52)
-
 print("#기호와 함께 출력하기")
-print(output_h)
-print(output_i)
+output_a="{:f}".format(52.273)
+output_b="{:15f}".format(52.273)
+output_c="{:+15f}".format(52.273)
+output_d="{:+015f}".format(52.273)
+
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+
+#소수점 아래 자릿수 지정하기
+print("#소수점 아래 자릿수 지정하기")
+output_j="{:15.3f}".format(52.273)
+output_z="{:15.2f}".format(52.273)
+output_g="{:15.1f}".format(52.273)
+
 print(output_j)
-print(output_k)
-print(output_l)
-print(output_m)
+print(output_z)
+print(output_g)
+
+
+#의미없는 소수점 제거하기
+print("#의미없는 소수점 제거하기")
+output_q=52.0
+output_w="{:g}".format(output_q)
+print(output_q)
+print(output_w)
