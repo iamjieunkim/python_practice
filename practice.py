2bed5b172f0e29410c6baa6a0e0155fa97ea69e7
jieun <jek3118@naver.com>
2022-11-12 01:02:59 +0900

    30

--- practice.py
+++ practice.py
@@ -1,23 +1,11 @@
-#정수를 특정 칸에 출력하기
-#정수
-output_a= "{:d}".format(52)
+#기호와 함께 출력하기
+output_f="{:+d}".format(52)
+output_g="{:+d}".format(-52)
+output_h="{: d}".format(52)
+output_i="{: d}".format(-52)
 
-#특정칸에 출력하기
-output_b= "{:5d}".format(52)
-output_c= "{:10d}".format(52)
-
-#빈칸을 0으로 채우기.
-output_d= "{:05d}".format(52)
-output_e= "{:05d}".format(-52)
-
-print("# 기본")
-print(output_a)
-
-print("#특정칸에출력하기")
-print(output_b)
-print(output_c)
-
-print("#빈칸을 0으로 채우기")
-print(output_d)
-print(output_e)
-
+print("#기호와 함께 출력하기")
+print(output_f)
+print(output_g)
+print(output_h)
+print(output_i)
