4d3ddbe815010a3b14988694f0833dc6b0423827
jieun <jek3118@naver.com>
2022-11-12 23:48:20 +0900

    39

--- practice.py
+++ practice.py
@@ -1,7 +1,20 @@
-#볼 만들기: 비교연산자
-print(10==100)
-print(10!=100)
-print(10>100)
-print(10<100)
-print(10>=100)
-print(10<=100)
+#볼 만들기: 논리연산자
+#not 연산자
+print(not True)
+print(not False)
+
+print("#not 연산자 조합하기")
+x=10
+under_20=x<20
+print("under_20: ", under_20)
+print("not under_20: ", not under_20)
+
+print("#and연산자와 or연산자")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
