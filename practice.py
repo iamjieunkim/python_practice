385fc696294624ba110dbcb413086762bd7db13d
jieun <jek3118@naver.com>
2022-11-13 00:26:45 +0900

    44-2

--- practice.py
+++ practice.py
@@ -2,12 +2,12 @@
 
 #입력을 받습니다.
 number = input("정수 입력> ")
-last_character=number[-1]
+number = int(number)
 
-#짝수 조건
-if last_character in "02468":
+#짝수조건
+if number%2 == 0:
     print("짝수입니다.")
 
-#홀수 조건
-if last_character in "13579":
+#홀수조건
+if number%2 == 1:
     print("홀수입니다.")
