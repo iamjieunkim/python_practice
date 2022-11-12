d3013adfbced1a5141dfeab79d73935c30371426
jieun <jek3118@naver.com>
2022-11-13 00:31:07 +0900

    45

--- practice.py
+++ practice.py
@@ -1,13 +1,11 @@
-#끝자리로 짝수와 홀수 구분
+#if조건문에 else구믄을 추가해서 짝수와 홀수 구분
 
 #입력을 받습니다.
-number = input("정수 입력> ")
-number = int(number)
+number=input("정수입력> ")
+number=int(number)
 
-#짝수조건
+#if조건문을 사용합니다.
 if number%2 == 0:
-    print("짝수입니다.")
-
-#홀수조건
-if number%2 == 1:
-    print("홀수입니다.")
+    print("짝수입니다.") #조건이 참일 때, 즉 짝수조건
+else:
+    print("홀수입니다.") #조건이 거짓일 때, 즉 홀수조건
