005af34e122ad06a3065f33327817d7be20e1341
jieun <jek3118@naver.com>
2022-11-13 00:01:32 +0900

    41-1

--- practice.py
+++ practice.py
@@ -6,9 +6,11 @@ import datetime
 now = datetime.datetime.now()
 
 #출력합니다
-print(now.year, "년")
-print(now.month, "월")
-print(now.day, "일")
-print(now.hour, "시")
-print(now.minute, "분")
-print(now.second, "초")
+print("{}년 {}월 {}일 {}시 {}분 {}초".format(
+    now.year,
+    now.month,
+    now.day,
+    now.hour,
+    now.minute,
+    now.second
+))
