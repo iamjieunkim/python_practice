80df2785586fcd0f7070883f37174162026b1188
jieun <jek3118@naver.com>
2022-11-13 00:04:32 +0900

    42

--- practice.py
+++ practice.py
@@ -1,16 +1,14 @@
-#날짜/시간 출력하기
+#오전과 오후를 구분하는 프로그램
 "#날짜/시간과 관련된 기능을 가져옵니다."
 import datetime
 
 #현재 날짜/시간을 구합니다."
 now = datetime.datetime.now()
 
-#출력합니다
-print("{}년 {}월 {}일 {}시 {}분 {}초".format(
-    now.year,
-    now.month,
-    now.day,
-    now.hour,
-    now.minute,
-    now.second
-))
+#오전 구분
+if now.hour < 12:
+    print("현재시각은 {}시로 오전입니다!".format(now.hour))
+
+#오후 구분
+if now.hour >= 12:
+    print("현재시각은 {}시로 오후입니다!".format(now.hour))
