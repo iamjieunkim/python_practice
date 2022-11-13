983c06a0dc008a4bc7c94317fa0ac5e49135ca4b
jieun <jek3118@naver.com>
2022-11-13 14:24:35 +0900

    51

--- practice.py
+++ practice.py
@@ -1,19 +1,13 @@
+#리스트에 요소 추가하기: append(), insert()
 #리스트를 선언합니다.
 list_a = [1, 2, 3]
-list_b = [4, 5, 6]
 
-#출력합니다.
-print("#리스트")
-print("list_a= ", list_a)
-print("list_b= ", list_b)
+print("#리스트 뒤에 요소추가하기")
+list_a.append(4)
+list_a.append(5)
+print(list_a)
 print()
 
-#기본연산자
-print("#리스트 기본연산자")
-print("list_a + list_b = ", list_a+list_b)
-print("list_a*3 =", list_a*3)
-print()
-
-#함수
-print('# 길이 구하기')
-print("len(list_a) = ", len(list_a))
+print("#리스트 중간에 요소추가하기")
+list_a.insert(0,10)
+print(list_a)
