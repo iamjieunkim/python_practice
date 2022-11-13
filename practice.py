b9f6dd1676a8664fed65ac3750a91e8cab736218
jieun <jek3118@naver.com>
2022-11-14 00:45:48 +0900

    63

--- practice.py
+++ practice.py
@@ -1,7 +1,4 @@
-#리스트와 범위를 조합해서 사용하기
-array=[273, 32, 103, 57, 52]
-
-#리스트에 반복문을 적용합니다.
-for i in range(len(array)):
+#반대로 반복하기
+for i in range(4, 0-1, -1):
     #출력합니다.
-    print("{}번째 반복: {}".format(i, array[i]))
+    print("현재 반복 변수: {}".format(i))
