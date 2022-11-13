44606caab957de971b5ca437e209cb88c89d39ee
jieun <jek3118@naver.com>
2022-11-13 21:05:20 +0900

    54-4

--- practice.py
+++ practice.py
@@ -1,11 +1,9 @@
 #4-1확인문제풀이
 
-#4
-list_of_list=[
-    [1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9]
-]
-for moment in list_of_list:
-    for item in moment:
-        print(item)
+#5
+numbers=[1, 2, 3, 4, 5, 6, 7, 8]
+output=[[],[],[]]
+for number in numbers:
+    output[(number+2)%3].append(number) #리스트에 요소 추가하기/ 리스트명.append(요소)
+
+print(output)
