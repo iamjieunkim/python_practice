01e78efd313d24ea287d9c38bc7c6258e14929db
jieun <jek3118@naver.com>
2022-11-14 00:42:42 +0900

    62

--- practice.py
+++ practice.py
@@ -1,12 +1,7 @@
-#for 반복문과 범위를 함께 조합해서 사용합니다.
-for i in range(5):
-    print(str(i)+"= 반복변수")
-print()
+#리스트와 범위를 조합해서 사용하기
+array=[273, 32, 103, 57, 52]
 
-for i in range(5, 10):
-    print(str(i)+"= 반복변수")
-print()
-
-for i in range(0, 10, 3):
-    print(str(i)+"= 반복변수")
-print()
+#리스트에 반복문을 적용합니다.
+for i in range(len(array)):
+    #출력합니다.
+    print("{}번째 반복: {}".format(i, array[i]))
