a729d8599e6ce99d8d645426fe00501730095877
jieun <jek3118@naver.com>
2022-11-13 21:19:38 +0900

    55

--- practice.py
+++ practice.py
@@ -1,9 +1,19 @@
-#4-1확인문제풀이
+#딕셔너리의 요소에 접근하기
+#딕셔너리를 선언합니다.
+dictionary={
+    "name": "7D 건조 망고",
+    "type": "당절임",
+    "ingredient": ["망고", "설탕", '메타중아황산나트륨', '치자황색소'],
+    "origin": "필리핀"
+}
 
-#5
-numbers=[1, 2, 3, 4, 5, 6, 7, 8]
-output=[[],[],[]]
-for number in numbers:
-    output[(number+2)%3].append(number) #리스트에 요소 추가하기/ 리스트명.append(요소)
+#출력합니다.
+print("name: ", dictionary["name"])
+print("type: ", dictionary["type"])
+print("ingredient: ", dictionary["ingredient"])
+print("origin: ", dictionary["origin"])
+print()
 
-print(output)
+#값을 변경합니다.
+dictionary["name"] = "8D 건조 망고"
+print("name:", dictionary["name"])
