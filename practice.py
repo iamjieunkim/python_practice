892bf45ca7f9ccad956613f900338d1e1069d0d1
jieun <jek3118@naver.com>
2022-11-14 01:56:32 +0900

    70-1

--- practice.py
+++ practice.py
@@ -1,10 +1,10 @@
 #4-3확인문제
-#1
-#숫지는 무작위로 입력해도 상관없습니다.
-key_list=["name", "hp", "mp", "level"]
-value_list=["기사", 200, 30, 5]
-character={}
-
-for i in range(len(key_list)):
-    character[key_list[i]]=value_list[i]
-print(character)
+#2
+limit=10000
+i=1
+#sum은 파이썬 내부에서 사용하는 식별자이므로 sum_value라는 변수 이름을 사용합니다.
+sum_value=0
+while sum_value < limit:
+    sum_value += i
+    i += 1
+print("{}를 더할 때 {}을 넘으며 그때의 값은 {}입니다.".format(i-1, limit, sum_value))
