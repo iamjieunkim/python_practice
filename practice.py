3d48e5963e19ab2a2014178de91e700625c5c60b
jieun <jek3118@naver.com>
2022-11-14 00:04:38 +0900

    60-2

--- practice.py
+++ practice.py
@@ -1,14 +1,24 @@
 #4-2 확인문제
 
-#2
-numbers=[1, 2, 6, 8, 4, 3, 2, 1, 9, 5, 4, 9, 7, 2, 1, 3, 5, 4, 8, 9, 7, 2, 3]
-counter = {}
+#3
+#딕셔너리를 선언합니다.
+character={
+    "name": "기사",
+    "level": 12,
+    "items": {
+        "sword": "불꽃의 검",
+        "armor": "풀플레이트"
+    },
+    "skill": ["베기", "세게 베기", "아주 세기 베기"]
+}
 
-for number in numbers:
-    if number in counter:
-        counter[number] = counter[number]+1
+#for 반복문을 사용합니다.
+for key in character:
+    if type(character[key]) is dict:
+        for small_key in character[key]:
+            print(small_key, ":", character[key][small_key])
+    elif type(character[key]) is list:
+        for item in character[key]:
+            print(key, ":", item)
     else:
-        counter[number]=1
-
-#최종 출력
-print(counter)
+        print(key, ":", character[key])
