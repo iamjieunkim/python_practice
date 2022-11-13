e496b201b6c4db72a943c0abf552fb255763ee90
jieun <jek3118@naver.com>
2022-11-13 14:27:22 +0900

    51-1

--- practice.py
+++ practice.py
@@ -1,13 +1,4 @@
-#리스트에 요소 추가하기: append(), insert()
-#리스트를 선언합니다.
-list_a = [1, 2, 3]
-
-print("#리스트 뒤에 요소추가하기")
-list_a.append(4)
-list_a.append(5)
-print(list_a)
-print()
-
-print("#리스트 중간에 요소추가하기")
-list_a.insert(0,10)
+#리스트에 한번에 여러 요소 추가하기
+list_a=[1, 2, 3]
+list_a.extend([4, 5, 6]) #append() 함수를 세번 반복한 효과
 print(list_a)
