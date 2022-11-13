d8c31b06af4858400845dee1729f1cf1b921af79
jieun <jek3118@naver.com>
2022-11-13 23:07:11 +0900

    57

--- practice.py
+++ practice.py
@@ -1,14 +1,16 @@
-#딕셔너리에 요소 추가하기
+#딕셔너리에 요소 제거하기
+
 #딕셔너리를 선언합니다.
-dictionary={}
+dictionary={
+    "name": "7D 건조 망고",
+    "type": "당절임"
+}
+#요소 제거 전에 내용을 출력해 봅니다.
+print("요소 제거 이전:", dictionary)
 
-#요소 추가 전에 내용을 출력해 봅니다.
-print("요소 추가 이전:", dictionary)
+#딕셔너리의 요소를 제거합니다.
+del dictionary["name"]
+del dictionary["type"]
 
-#딕셔너리에 요소를 추가합니다.
-dictionary["name"]="새로운 이름"
-dictionary['head']="새로운 정신"
-dictionary["body"]="새로운 몸"
-
-#출력합니다.
-print("요소 추가 이후:", dictionary)
+#요소 제거 후에 내용을 출력해 봅니다.
+print("요소 제거 이후:", dictionary)
