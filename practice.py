b711c08cdb72fc0994adf9b30873485f4c6bc486
jieun <jek3118@naver.com>
2022-11-13 14:16:45 +0900

    50

--- practice.py
+++ practice.py
@@ -1,14 +1,19 @@
-#pass 키워드를 사용한 미구현 부분 입력
+#리스트를 선언합니다.
+list_a = [1, 2, 3]
+list_b = [4, 5, 6]
 
-#입력을 받습니다.
-number=input("정수입력> ")
-number=int(number)
+#출력합니다.
+print("#리스트")
+print("list_a= ", list_a)
+print("list_b= ", list_b)
+print()
 
-#조건문 사용
-if number > 0:
-    #양수일 때: 아직 미구현 상태입니다.
-    raise NotImplementedError
+#기본연산자
+print("#리스트 기본연산자")
+print("list_a + list_b = ", list_a+list_b)
+print("list_a*3 =", list_a*3)
+print()
 
-else:
-    #음수일 떄: 아직 미구현 상태입니다.
-    raise NotImplementedError
+#함수
+print('# 길이 구하기')
+print("len(list_a) = ", len(list_a))
