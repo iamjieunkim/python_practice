56cf67db18bc868db58e5bc18200ea46420b24a6
jieun <jek3118@naver.com>
2022-11-14 00:59:33 +0900

    66

--- practice.py
+++ practice.py
@@ -1,5 +1,10 @@
-#while 반복문을 for반복문 처럼 사용하기
-i=0
-while i <10:
-    print("{}번째 반복입니다.".format(i))
-    i+=1
+#해당하는 값 모두 제거하기
+#변수를 선언합니다.
+list_test=[1, 2, 1, 2]
+value = 2
+
+#list_text 내부에 value값이 있다면 반복
+while value in list_test:
+    list_test.remove(value)
+
+print(list_test)
