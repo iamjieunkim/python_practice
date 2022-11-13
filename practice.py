88ff1734373a7f5008de8e2775f9aab6913e9ddd
jieun <jek3118@naver.com>
2022-11-13 14:53:29 +0900

    53

--- practice.py
+++ practice.py
@@ -1,21 +1,12 @@
-#리스트 연결 연산자와 요소 추가의 차이
-list_a=[0, 1, 2, 3, 4, 5]
-print("# 리스트의 요소 하나 제거하기")
+#for 반복문과 리스트
+#리스트를 선업합니다.
+array=[273, 32, 103, 57, 52]
 
-#제거방법[1]- del
-del list_a[1]
-print("del list_a[1]: ", list_a)
+#리스트에 반복문을 적용합니다.
+for element in array:
+    #출력합니다.
+    print(element)
 
-#제거방법[2]- pop()
-list_a.pop(2)
-print("pop(2): ", list_a)
 
-#값으로 제거하기:remove()
-list_c=[1, 2, 1, 2]
-list_c.remove(2)
-print(list_c)
-
-#모두 제거하기:clear()
-list_d=[0, 1, 2, 3, 4, 5]
-list_d.clear()
-print(list_d)
+for character in "안녕하세요":
+    print("-", character)
