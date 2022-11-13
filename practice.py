6a41a792eaaf5f8ffaa900f9aa76574d26e4cf21
jieun <jek3118@naver.com>
2022-11-13 23:44:49 +0900

    60-1

--- practice.py
+++ practice.py
@@ -1,13 +1,14 @@
 #4-2 확인문제
 
-#1
-pets = [
-    {"name": "구름", "age": 5},
-    {"name": "초코", "age": 3},
-    {"name": "아지", "age": 1},
-    {"name": "호랑이", "age": 1}
-]
+#2
+numbers=[1, 2, 6, 8, 4, 3, 2, 1, 9, 5, 4, 9, 7, 2, 1, 3, 5, 4, 8, 9, 7, 2, 3]
+counter = {}
 
-print("#우리 동네 애완 동물")
-for pet in pets:
-    print(pet["name"], str(pet["age"])+"살")
+for number in numbers:
+    if number in counter:
+        counter[number] = counter[number]+1
+    else:
+        counter[number]=1
+
+#최종 출력
+print(counter)
