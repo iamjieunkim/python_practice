1e322661af3c220a6443cf5da9060b1a5e64b24e
jieun <jek3118@naver.com>
2022-11-14 00:36:41 +0900

    61

--- practice.py
+++ practice.py
@@ -1,24 +1,12 @@
-#4-2 확인문제
+#for 반복문과 범위를 함께 조합해서 사용합니다.
+for i in range(5):
+    print(str(i)+"= 반복변수")
+print()
 
-#3
-#딕셔너리를 선언합니다.
-character={
-    "name": "기사",
-    "level": 12,
-    "items": {
-        "sword": "불꽃의 검",
-        "armor": "풀플레이트"
-    },
-    "skill": ["베기", "세게 베기", "아주 세기 베기"]
-}
+for i in range(5, 10):
+    print(str(i)+"= 반복변수")
+print()
 
-#for 반복문을 사용합니다.
-for key in character:
-    if type(character[key]) is dict:
-        for small_key in character[key]:
-            print(small_key, ":", character[key][small_key])
-    elif type(character[key]) is list:
-        for item in character[key]:
-            print(key, ":", item)
-    else:
-        print(key, ":", character[key])
+for i in range(0, 10, 3):
+    print(str(i)+"= 반복변수")
+print()
