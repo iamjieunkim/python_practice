dc6d68770ef6856eb358fc89d4511f94f40981a3
jieun <jek3118@naver.com>
2022-11-13 20:56:26 +0900

    54-1

--- practice.py
+++ practice.py
@@ -1,8 +1,10 @@
 #4-1확인문제풀이
 
-#1
+#2
 numbers=[273, 103, 5, 32, 65, 9, 72, 800, 99]
 
 for number in numbers:
-    if number>=100:
-        print("100이상의 수: ", number)
+    if number%2 == 1:
+        print(number, "는 홀수입니다.")
+    else:
+        print(number, "는 짝수입니다.")
