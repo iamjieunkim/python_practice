b624078c163f421559d81f3598623badad6dcfdd
jieun <jek3118@naver.com>
2022-11-13 23:09:57 +0900

    58

--- practice.py
+++ practice.py
@@ -1,16 +1,18 @@
-#딕셔너리에 요소 제거하기
+#키가 존재하는지 확인하고 값에 접근하기
 
 #딕셔너리를 선언합니다.
 dictionary={
     "name": "7D 건조 망고",
-    "type": "당절임"
+    "type": "당절임",
+    "ingredient": ["망고", "설탕", '메타중아황산나트륨', '치자황색소'],
+    "origin": "필리핀"
 }
-#요소 제거 전에 내용을 출력해 봅니다.
-print("요소 제거 이전:", dictionary)
 
-#딕셔너리의 요소를 제거합니다.
-del dictionary["name"]
-del dictionary["type"]
+#사용자로 입력을 받습니다.
+key=input(">접근하고자 하는 키: ")
 
-#요소 제거 후에 내용을 출력해 봅니다.
-print("요소 제거 이후:", dictionary)
+#출력합니다.
+if key in dictionary:
+    print(dictionary[key])
+else:
+    print("존재하지 않는 키에 접근하고 있습니다.")
