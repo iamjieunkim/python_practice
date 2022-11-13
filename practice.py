3c55b87fc0b22457d298d060b0e0a843b9add017
jieun <jek3118@naver.com>
2022-11-13 14:41:45 +0900

    52-1

--- practice.py
+++ practice.py
@@ -9,3 +9,13 @@ print("del list_a[1]: ", list_a)
 #제거방법[2]- pop()
 list_a.pop(2)
 print("pop(2): ", list_a)
+
+#값으로 제거하기:remove()
+list_c=[1, 2, 1, 2]
+list_c.remove(2)
+print(list_c)
+
+#모두 제거하기:clear()
+list_d=[0, 1, 2, 3, 4, 5]
+list_d.clear()
+print(list_d)
