b20c45bb07378baacd5768f191669e9379c48b69
jieun <jek3118@naver.com>
2022-11-13 20:58:50 +0900

    54-2

--- practice.py
+++ practice.py
@@ -1,10 +1,7 @@
 #4-1확인문제풀이
 
-#2
+#3
 numbers=[273, 103, 5, 32, 65, 9, 72, 800, 99]
 
 for number in numbers:
-    if number%2 == 1:
-        print(number, "는 홀수입니다.")
-    else:
-        print(number, "는 짝수입니다.")
+    print(number, "는", len(str(number)), "자릿수입니다.")
