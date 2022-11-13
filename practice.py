dc7221f790e2f29555aaf58c91d4138f5bda4e23
jieun <jek3118@naver.com>
2022-11-13 21:00:58 +0900

    54-3

--- practice.py
+++ practice.py
@@ -1,7 +1,11 @@
 #4-1확인문제풀이
 
-#3
-numbers=[273, 103, 5, 32, 65, 9, 72, 800, 99]
-
-for number in numbers:
-    print(number, "는", len(str(number)), "자릿수입니다.")
+#4
+list_of_list=[
+    [1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9]
+]
+for moment in list_of_list:
+    for item in moment:
+        print(item)
