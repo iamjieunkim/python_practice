7b4ac7ff159b24f36915ce8538f37c2e51c72d8b
jieun <jek3118@naver.com>
2022-11-14 23:53:02 +0900

    74-1

--- practice.py
+++ practice.py
@@ -1,10 +1,7 @@
-#반복문을 사용한 리스트 생성
+#리스트 안에 for문 사용하기
 
 #변수를 선언합니다.
-array=[]
+array=[i*i for i in range(0, 20, 2)]
 
-#반복문을 적용합니다.
-for i in range(0, 20, 2):
-    array.append(i*i)
 #출력합니다.
 print(array)
