e973f58ad084a280932c4393b81f9ce44191c5c3
jieun <jek3118@naver.com>
2022-11-14 23:59:15 +0900

    75

--- practice.py
+++ practice.py
@@ -1,7 +1,8 @@
-#리스트 안에 for문 사용하기
+#조건을 활용한 리스트 내포
 
-#변수를 선언합니다.
-array=[i*i for i in range(0, 20, 2)]
+#리스트를 선언합니다.
+array=["사과", "자두", "초콜릿", "바나나", "체리"]
+output=[fruit for fruit in array if fruit != "초콜릿"]
 
 #출력합니다.
 print(array)
