17d562af9db71b4aa6009b90ed43c5bf2eef8822
jieun <jek3118@naver.com>
2022-11-14 23:51:31 +0900

    74

--- practice.py
+++ practice.py
@@ -1,18 +1,10 @@
-#딕셔너리의 items() 함수와 반복문
+#반복문을 사용한 리스트 생성
+
 #변수를 선언합니다.
-example_dictionary = {
-    "키A": "값A",
-    "키B": "값B",
-    "키C": "값C"
-}
+array=[]
 
-#딕셔너리의 items()함수 결과 출력하기
-print("# 딕셔너리의 items()함수")
-print("items():", example_dictionary.items())
-print()
-
-#for 반복문과 items() 함수 조합해서 사용하기
-print("# 딕셔너리의 items()함수와 반복문 조합하기")
-
-for key, element in example_dictionary.items():
-    print("dictionary[{}] = {}".format(key, element))
+#반복문을 적용합니다.
+for i in range(0, 20, 2):
+    array.append(i*i)
+#출력합니다.
+print(array)
