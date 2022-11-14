46fb897fb26cd4a9f5732e76e044969e7ad9646b
jieun <jek3118@naver.com>
2022-11-14 23:21:44 +0900

    71

--- practice.py
+++ practice.py
@@ -1,15 +1,17 @@
-#4-3확인문제
-#3
-max_value=0
-a=0
-b=0
+#reversed() 함수
 
-for i in (1, 100//2+1):
-    j = 100 - i
-    if max_value < i * j:
-        max_value = i * j
-        a = i
-        b = j
+#리스트를 선언하고 뒤집습니다.
+list_a=[1, 2, 3, 4, 5]
+list_reversed=reversed(list_a)
 
-print("최대가 되는 경우: {} * {} = {}".format(a, b, max_value))
+#출력합니다.
+print("# reversed() 함수")
+print("reversed([1, 2, 3, 4, 5] :", list_reversed)
+print("list(reversed([1, 2, 3, 4, 5])):", list(list_reversed))
+print()
 
+#반복문을 적용해 봅시다.
+print("# reversed() 함수와 반복문")
+print("for i in reversed([1, 2, 3, 4, 5])")
+for i in reversed(list_a):
+    print("-", i)
